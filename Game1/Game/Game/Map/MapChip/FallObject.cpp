242546167fb3279afe30cb4d72eacc6097bf2441
yuuki_fujii <a>
2018-02-16 20:35:34 +0900

    修正

--- Game1/Game/Game/Map/MapChip/FallObject.cpp
+++ Game1/Game/Game/Map/MapChip/FallObject.cpp
@@ -33,22 +33,22 @@ void FallObject::Init(const D3DXVECTOR3& position, const D3DXQUATERNION& rotatio
 	m_rigidBody.SetPlayerCollisionGroundFlg(false);
 
 	//パーティクルを初期化
-	m_particle = New<ParticleEmitter>(CAMERA_PRIORITY);
-	m_particle->Init({
-		"Assets/particle/WallDust.png",						//テクスチャのファイルパス
-		0.4f,												//パーティクルの横幅
-		0.4f,												//パーティクルの縦幅
-		{ 0.0f, 0.0f, 1.0f, 1.0f },							//テクスチャのuv。xyが左上のuvでzwが右下のuv
-		{ aabb.x, 3.0f, aabb.z },					//パーティクルの座標のランダム幅
-		{ 0.0f, -0.5f, 0.0f },								//パーティクルの重力
-		1.3f,												//パーティクルの寿命
-		1.0f,												//パーティクルが出るまでのインターバル
-		0.0f,												//エミッターの寿命
-		{ m_position.x, m_position.y - 11.0f, m_position.z },//エミッターの座標
-		2,													//1フレームで出るパーティクルの数
-		true												//最初にパーティクルをエミットする時だけ時間をランダムにするか
-	}
-	, &GetGameScene().GetCamera());
+	//m_particle = New<ParticleEmitter>(CAMERA_PRIORITY);
+	//m_particle->Init({
+	//	"Assets/particle/WallDust.png",						//テクスチャのファイルパス
+	//	0.4f,												//パーティクルの横幅
+	//	0.4f,												//パーティクルの縦幅
+	//	{ 0.0f, 0.0f, 1.0f, 1.0f },							//テクスチャのuv。xyが左上のuvでzwが右下のuv
+	//	{ aabb.x, 3.0f, aabb.z },					//パーティクルの座標のランダム幅
+	//	{ 0.0f, -0.5f, 0.0f },								//パーティクルの重力
+	//	1.3f,												//パーティクルの寿命
+	//	1.0f,												//パーティクルが出るまでのインターバル
+	//	0.0f,												//エミッターの寿命
+	//	{ m_position.x, m_position.y - 11.0f, m_position.z },//エミッターの座標
+	//	2,													//1フレームで出るパーティクルの数
+	//	true												//最初にパーティクルをエミットする時だけ時間をランダムにするか
+	//}
+	//, &GetGameScene().GetCamera());
 	m_skinModel.SetShaderTechnique(enShaderTechniqueDithering);
 }
 
